13f25b6e6af2f76e9c471f76549d1d9db7542582
aarvros <awclose02@gmail.com>
2025-07-05 17:31:18 -0500

    Added loading dialog during ffmpeg

---
+++ MTVBAPlus/LoadingDialog.xaml.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace MTVBAPlus
+{
+    public partial class LoadingDialog : Window
+    {
+        public LoadingDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
